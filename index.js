49249e25bd684095a33ef2bfc409aa5ce77ee29f
MiwakoHerai <m.herai.jg@gmail.com>
2023-06-07 09:12:56 +0900

    ユーザー情報取得に成功

--- index.js
+++ index.js
@@ -8,17 +8,38 @@ const request = require('request')
 const express = require('express')
 const app = express()
 
+// Function to get meeting information
+function getMeetingInfo(accessToken, callback) {
+    // Set the API endpoint URL
+    const apiUrl = 'https://api.zoom.us/v2/users/me/meetings/participants'
+
+    // Set the request headers
+    const headers = {
+        'Authorization': 'Bearer ' + accessToken
+    }
+
+    // Send GET request to the API endpoint
+    request.get({ url: apiUrl, headers: headers }, (error, response, body) => {
+        if (error) {
+            console.log('API Response Error: ', error)
+            callback(error, null)
+        } else {
+            body = JSON.parse(body)
+            callback(null, body)
+        }
+    })
+}
 
 app.get('/', (req, res) => {
 
-    // Step 1: 
-    // Check if the code parameter is in the url 
-    // if an authorization code is available, the user has most likely been redirected from Zoom OAuth
-    // if not, the user needs to be redirected to Zoom OAuth to authorize
+    // Step 1:
+    // Check if the code parameter is in the URL
+    // If an authorization code is available, the user has most likely been redirected from Zoom OAuth
+    // If not, the user needs to be redirected to Zoom OAuth to authorize
 
     if (req.query.code) {
 
-        // Step 3: 
+        // Step 3:
         // Request an access token using the auth code
 
         let url = 'https://zoom.us/oauth/token?grant_type=authorization_code&code=' + req.query.code + '&redirect_uri=' + process.env.redirectURL;
@@ -31,7 +52,7 @@ app.get('/', (req, res) => {
             // Logs your access and refresh tokens in the browser
             console.log(`access_token: ${body.access_token}`);
             console.log(`refresh_token: ${body.refresh_token}`);
-
+            const access_token = body.access_token;
             if (body.access_token) {
 
                 // Step 4:
@@ -48,30 +69,112 @@ app.get('/', (req, res) => {
                         body = JSON.parse(body);
                         // Display response in console
                         console.log('API call ', body);
-                        // Display response in browser
-                        var JSONResponse = '<pre><code>' + JSON.stringify(body, null, 2) + '</code></pre>'
-                        res.send(`
-                            <style>
-                                @import url('https://fonts.googleapis.com/css?family=Open+Sans:400,600&display=swap');@import url('https://necolas.github.io/normalize.css/8.0.1/normalize.css');html {color: #232333;font-family: 'Open Sans', Helvetica, Arial, sans-serif;-webkit-font-smoothing: antialiased;-moz-osx-font-smoothing: grayscale;}h2 {font-weight: 700;font-size: 24px;}h4 {font-weight: 600;font-size: 14px;}.container {margin: 24px auto;padding: 16px;max-width: 720px;}.info {display: flex;align-items: center;}.info>div>span, .info>div>p {font-weight: 400;font-size: 13px;color: #747487;line-height: 16px;}.info>div>span::before {content: "👋";}.info>div>h2 {padding: 8px 0 6px;margin: 0;}.info>div>p {padding: 0;margin: 0;}.info>img {background: #747487;height: 96px;width: 96px;border-radius: 31.68px;overflow: hidden;margin: 0 20px 0 0;}.response {margin: 32px 0;display: flex;flex-wrap: wrap;align-items: center;justify-content: space-between;}.response>a {text-decoration: none;color: #2D8CFF;font-size: 14px;}.response>pre {overflow-x: scroll;background: #f6f7f9;padding: 1.2em 1.4em;border-radius: 10.56px;width: 100%;box-sizing: border-box;}
-                            </style>
-                            <div class="container">
-                                <div class="info">
-                                    <img src="${body.pic_url}" alt="User photo" />
-                                    <div>
-                                        <span>Hello World!</span>
-                                        <h2>${body.first_name} ${body.last_name}</h2>
-                                        <p>${body.role_name}, ${body.company}</p>
-                                    </div>
-                                </div>
-                                <div class="response">
-                                    <h4>JSON Response:</h4>
-                                    <a href="https://marketplace.zoom.us/docs/api-reference/zoom-api/users/user" target="_blank">
-                                        API Reference
-                                    </a>
-                                    ${JSONResponse}
-                                </div>
-                            </div>
-                        `);
+
+                        // Get meeting information using the access token
+                        getMeetingInfo(access_token, (error, meetings) => {
+                            if (error) {
+                                // Handle error
+                            } else {
+                                // Display response in console
+                                console.log('Meeting Info: ', meetings);
+
+                                // Display response in browser
+                                var JSONResponse = '<pre><code>' + JSON.stringify(meetings, null, 2) + '</code></pre>'
+                                res.send(`
+                  <style>
+                    @import url('https://fonts.googleapis.com/css?family=Open+Sans:400,600&display=swap');
+                    @import url('https://necolas.github.io/normalize.css/8.0.1/normalize.css');
+                    html {
+                      color: #232333;
+                      font-family: 'Open Sans', Helvetica, Arial, sans-serif;
+                      -webkit-font-smoothing: antialiased;
+                      -moz-osx-font-smoothing: grayscale;
+                    }
+                    h2 {
+                      font-weight: 700;
+                      font-size: 24px;
+                    }
+                    h4 {
+                      font-weight: 600;
+                      font-size: 14px;
+                    }
+                    .container {
+                      margin: 24px auto;
+                      padding: 16px;
+                      max-width: 720px;
+                    }
+                    .info {
+                      display: flex;
+                      align-items: center;
+                    }
+                    .info > div > span,
+                    .info > div > p {
+                      font-weight: 400;
+                      font-size: 13px;
+                      color: #747487;
+                      line-height: 16px;
+                    }
+                    .info > div > span::before {
+                      content: "👋";
+                    }
+                    .info > div > h2 {
+                      padding: 8px 0 6px;
+                      margin: 0;
+                    }
+                    .info > div > p {
+                      padding: 0;
+                      margin: 0;
+                    }
+                    .info > img {
+                      background: #747487;
+                      height: 96px;
+                      width: 96px;
+                      border-radius: 31.68px;
+                      overflow: hidden;
+                      margin: 0 20px 0 0;
+                    }
+                    .response {
+                      margin: 32px 0;
+                      display: flex;
+                      flex-wrap: wrap;
+                      align-items: center;
+                      justify-content: space-between;
+                    }
+                    .response > a {
+                      text-decoration: none;
+                      color: #2D8CFF;
+                      font-size: 14px;
+                    }
+                    .response > pre {
+                      overflow-x: scroll;
+                      background: #f6f7f9;
+                      padding: 1.2em 1.4em;
+                      border-radius: 10.56px;
+                      width: 100%;
+                      box-sizing: border-box;
+                    }
+                  </style>
+                  <div class="container">
+                    <div class="info">
+                      <img src="${body.pic_url}" alt="User photo" />
+                      <div>
+                        <span>Hello World!</span>
+                        <h2>${body.first_name} ${body.last_name}</h2>
+                        <p>${body.role_name}, ${body.company}</p>
+                      </div>
+                    </div>
+                    <div class="response">
+                      <h4>JSON Response:</h4>
+                      <a href="https://marketplace.zoom.us/docs/api-reference/zoom-api/users/user" target="_blank">
+                        API Reference
+                      </a>
+                      ${JSONResponse}
+                    </div>
+                  </div>
+                `);
+                            }
+                        });
+
                     }
                 }).auth(null, null, true, body.access_token);
 
@@ -85,9 +188,9 @@ app.get('/', (req, res) => {
 
     }
 
-    // Step 2: 
+    // Step 2:
     // If no authorization code is available, redirect to Zoom OAuth to authorize
     res.redirect('https://zoom.us/oauth/authorize?response_type=code&client_id=' + process.env.clientID + '&redirect_uri=' + process.env.redirectURL)
 })
 
-app.listen(4000, () => console.log(`Zoom Hello World app listening at PORT: 4000`))
+app.listen(33333, () => console.log(`Zoom Hello World app listening at PORT: 33333`))
